6fbd53ccbf3e91d6466c93fc4b4882a88798badb
Yoonji Heo <myeunee@gmail.com>
2025-10-14 18:03:14 +0900

    feat: Identity Token과 Auth Code를 사용하여 로그인 처리

--- src/main/java/com/sbpb/ddobak/server/domain/auth/dto/AppleLoginRequest.java
+++ src/main/java/com/sbpb/ddobak/server/domain/auth/dto/AppleLoginRequest.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 /**
  * Apple 로그인 요청 DTO
- * Apple Identity Token만으로 로그인 처리
+ * Apple Identity Token과 Authorization Code를 사용하여 로그인 처리
  */
 @Getter
 @Setter
@@ -20,4 +20,11 @@ public class AppleLoginRequest {
      */
     @NotBlank(message = "Identity token is required")
     private String identityToken;
+    
+    /**
+     * Apple Authorization Code (선택사항)
+     * Refresh Token 발급을 위해 필요
+     * 탈퇴 시 Apple 서버에서 계정 삭제를 위해 사용됨
+     */
+    private String authorizationCode;
 } 
